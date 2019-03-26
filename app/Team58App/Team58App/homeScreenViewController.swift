36f1eff5e170684714cc635158f37ef8c9e4c696
rpoplaws <rpoplaws@iu.edu>
2019-03-26 05:14:46 -0400

    beckys recent changes

--- app/Team58App/Team58App/homeScreenViewController.swift
+++ app/Team58App/Team58App/homeScreenViewController.swift
@@ -11,27 +11,6 @@ import UIKit
 
 class homeScreenViewController: UIViewController {
 
-    @IBOutlet weak var labelEmail: UITextField!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        
-        //getting user data from defaults
-        let defaultValues = UserDefaults.standard
-        if let name = defaultValues.string(forKey: "email"){
-            //setting the name to label
-            labelEmail.text = name
-        }else{
-            //send back to login view controller
-        }
-        
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     
 }
